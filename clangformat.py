0471c6540a9274077e8f93396db9fd14399ebc9f
skellykiernan <skellykiernan@users.noreply.github.com>
2019-10-14 12:03:35 +0100

    Hide console appearing

--- clangformat.py
+++ clangformat.py
@@ -4,19 +4,19 @@ import sublime_plugin
 
 import subprocess
 
-# does a simple reformat, 
-# via a linux server(samba) not sure it will 
+# does a simple reformat if a .clang_format file is present and the current file is saved
 # and also add a key binding, e.g. { "keys": ["ctrl+alt+f"], "command": "cformat" },
 class CformatCommand(sublime_plugin.TextCommand):
     def run(self, edit):
         filename = self.view.file_name()
         print(filename)
         shell_cmd = ["C:\\Program Files\\LLVM\\bin\\clang-format", "-i", filename]
-        self.proc = subprocess.Popen(shell_cmd)
-        #reloads the window, not sure if a delay will be needed, e.g. time.sleep(0.1)
+        # hoping the wait will till file is written where requesting the window loads the saved file
+        # if on a netwrok drive likely to be a noticable delay
+        self.proc = subprocess.Popen(shell_cmd, shell=True).wait()
         self.view.window()
 
-        
+
 
 
 
