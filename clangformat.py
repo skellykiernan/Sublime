ddfc6e82562f090c83d8ad71b14bb916859e10f8
Skelly <skelly@nothere.com>
2019-11-23 16:26:36 +0000

    Add Plugin settings

--- clangformat.py
+++ clangformat.py
@@ -7,10 +7,19 @@ import subprocess
 # does a simple reformat if a .clang_format file is present and the current file is saved
 # and also add a key binding, e.g. { "keys": ["ctrl+alt+f"], "command": "cformat" },
 class CformatCommand(sublime_plugin.TextCommand):
+    def __init__(self, view):
+        
+        # At moment must be set 
+        settings = sublime.load_settings('DerekPlugins.sublime-settings')
+        self.clang_format_path = settings.get('clang_format_path')
+
+        # Note I have overwritten a constuctor this works not sure why
+        self.view = view
+
     def run(self, edit):
         filename = self.view.file_name()
         print(filename)
-        shell_cmd = ["C:\\Program Files\\LLVM\\bin\\clang-format", "-style=file",  "-i",  "-fallback-style=none", filename]
+        shell_cmd = [self.clang_format_path, "-style=file",  "-i",  "-fallback-style=none", filename]
         # hoping the wait will till file is written where requesting the window loads the saved file
         # if on a netwrok drive likely to be a noticable delay
         self.proc = subprocess.Popen(shell_cmd, shell=True).wait()
--- vim.py
+++ vim.py
@@ -4,20 +4,27 @@ import sublime_plugin
 import subprocess
 
 
-class Open_in_vimCommand(sublime_plugin.TextCommand):
+class Open_in_vimCommand(sublime_plugin.TextCommand): 
+    def __init__(self, view):
+        # At moment must be set 
+        settings = sublime.load_settings('DerekPlugins.sublime-settings')
+        self.gvim_path = settings.get('gvim_path')
+
+        # Note I have overwritten a constuctor this works not sure why, change settings
+        self.view = view
+
     def run(self, edit):
         filename = self.view.file_name()
 
         sel  = self.view.sel()
         # the first selection that maybe iterated over, adding 1 for to ensure start line numbering from 1 rather than 0
         line_sel  = self.view.rowcol(sel[0].begin())[0] + 1
-        print(line_sel)
 
         if filename == None:
-            shell_cmd = ["C:\\Program Files (x86)\\Vim\\vim80\\gvim.exe"]
+            shell_cmd = [self.gvim_path]
         else:
             line_sel_str = "+" + str(line_sel)
-            shell_cmd = ["C:\\Program Files (x86)\\Vim\\vim80\\gvim.exe", "--remote-silent", line_sel_str, filename]
+            shell_cmd = [self.gvim_path, "--remote-silent", line_sel_str, filename]
 
         print(shell_cmd)
 
